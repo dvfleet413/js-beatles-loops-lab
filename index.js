6300e0d6aa020784b6435743d7c5a0d3a96483b1
Dave Van Fleet <dvfleet413@gmail.com>
2019-12-06 23:22:39 -0500

    Completed "Beatles Loop Lab"

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var array = [];
+  for(var i = 0; i < musicians.length; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array;
+}
+
+function johnLennonFacts(facts){
+  var i = 0;
+  var newArray = [];
+  while (i < facts.length){
+    newArray.push(`${facts[i]}!!!`);
+    i++
+  }
+  return newArray;
+}
+
+function iLoveTheBeatles(num){
+  var newArray = [];
+  //Implement a do-while loop inside the function that adds "I love the Beatles!" to the empty array.
+  do{
+    newArray.push('I love the Beatles!');
+    num++;
+  }while (num < 15)
+  return newArray;
+}
